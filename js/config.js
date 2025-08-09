72ffbf152b2ba3bd4fc9e050945717304430f482
Mario Alfonso Esquivel <165584194+MarioEsquivelIII@users.noreply.github.com>
2025-08-09 15:53:50 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_wuh5BRVHH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2a6evlqd9or9kg2qbhfqcvf98b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
